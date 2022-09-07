afb9db4b10c4f51deb04c5b45afb6f7224754ef5
Tam Nguyen <npt.dc@outlook.com>
2022-09-07 12:13:05 +0700

    ✨ Add jaffle shop stagging models

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,1 +1,1 @@
-select id as customer_id, first_name, last_name from `dbt-tutorial`.jaffle_shop.customers
+select id as customer_id, first_name, last_name from {{ source("jaffle_shop", "customers") }}
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,1 +1,1 @@
-select id as order_id, user_id as customer_id, order_date, status from `dbt-tutorial`.jaffle_shop.orders
+select id as order_id, user_id as customer_id, order_date, status from {{ source("jaffle_shop", "orders") }}
