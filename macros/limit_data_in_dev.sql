b08dc14ab53d4483510835f215f1413a566b404d
Tam Nguyen <tam.nguyen@flownetworks.io>
2022-10-10 15:27:48 +0000

    🔧 Update macros

--- macros/limit_data_in_dev.sql
+++ macros/limit_data_in_dev.sql
@@ -1,5 +1,5 @@
 {% macro limit_data_in_dev(column_name, dev_days_of_data=5) -%}
-    {%- if target.name == 'dev' %}
+    {%- if target.name == 'dev' -%}
         where {{ column_name }} >= date_sub(current_date(), interval {{ dev_days_of_data }} day)
     {% endif %}
 {%- endmacro %}
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,2 +1,2 @@
 select id as order_id, user_id as customer_id, order_date, status from {{ source("jaffle_shop", "orders") }}
-    {{ limit_data_in_dev(column_name='order_date', dev_days_of_data=1000) }}
+{{ limit_data_in_dev(column_name='order_date', dev_days_of_data=2000) }}
