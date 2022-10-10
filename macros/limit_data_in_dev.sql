2d748f60483fc2918c6dec5c490e75588e71188e
Tam Nguyen <npt.dc@outlook.com>
2022-10-10 22:19:30 +0700

    ✨ Add macro examples

--- macros/limit_data_in_dev.sql
+++ macros/limit_data_in_dev.sql
@@ -1,5 +1,5 @@
-{% macro limit_data_in_dev(column_name, dev_days_of_data=3) -%}
+{% macro limit_data_in_dev(column_name, dev_days_of_data=5) -%}
     {%  if target.name == 'dev' %}
-        where {{ column_name }} >= date_add('day', -{{ dev_days_of_data }}, current_timestamp)
+        where {{ column_name }} >= date_sub(current_date(), interval {{ dev_days_of_data }} day)
     {% endif %}
 {%- endmacro %}
