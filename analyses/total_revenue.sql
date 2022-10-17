b8fe6d0069325efc5de240eae03a67268e8d9fe5
Tam Nguyen <npt.dc@outlook.com>
2022-10-17 09:14:44 +0700

    ✨ Add sample analyses

---
+++ analyses/total_revenue.sql
@@ -0,0 +1,5 @@
+with
+    payments as (select * from {{ ref("stg_stripe__payments") }}),
+    aggregated as (select sum(amount) as total_revenue from payments where status = 'success')
+select *
+from aggregated
